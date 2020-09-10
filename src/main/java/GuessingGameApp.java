c14730fa29f1055e970aa5053f742e78df3ad03b
Kolton Koester <koltonkoester10@gmail.com>
2020-09-10 17:24:03 -0400

    finished 3rd iteration

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -5,11 +5,20 @@ public class GuessingGameApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please guess a number 1-10: ");
+        System.out.println("If you need understanding on how to play enter \" 0 \".");
         int guess = input.nextInt();
-        if (guess == 7){
-            System.out.println("You win!");
-        } else {
-            System.out.println( "You lose!");
+        for(int triedAttempts = 0; triedAttempts < 2; triedAttempts++) {
+            if (guess == 7) {
+                System.out.println("You win!");
+                break;
+            } else if (guess == 0) {
+                System.out.println("Please enter a number 1-10 until you win!");
+            } else if (guess != 7){
+                System.out.println("You have one more try!");
+                guess = input.nextInt();
+            } else {
+                System.out.println("You lose!");
+            }
         }
     }
 
