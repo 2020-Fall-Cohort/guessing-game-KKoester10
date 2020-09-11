2b4f8e029a4297957f3d5e397f158126b9438e9b
Kolton Koester <koltonkoester10@gmail.com>
2020-09-10 20:09:08 -0400

    editing iterations 2 and 3, but finished iteration 4

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -4,21 +4,31 @@ public class GuessingGameApp {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please guess a number 1-10: ");
         System.out.println("If you need understanding on how to play enter \" 0 \".");
+        System.out.println("Please guess a number 1-10: ");
         int guess = input.nextInt();
-        for(int triedAttempts = 0; triedAttempts < 2; triedAttempts++) {
+        int triedAttempts = 0;
+        while (triedAttempts < 1){
             if (guess == 7) {
                 System.out.println("You win!");
                 break;
             } else if (guess == 0) {
-                System.out.println("Please enter a number 1-10 until you win!");
-            } else if (guess != 7){
-                System.out.println("You have one more try!");
+                System.out.println("You enter a number 1-10 until you win!");
+                System.out.println("Please guess a number 1-10: ");
                 guess = input.nextInt();
+
+            } else if(guess < 0) {
+                System.out.println("You Entered A Negative Integer :( Sorry.");
+                System.out.println("GoodBye!");
+                break;
             } else {
-                System.out.println("You lose!");
+                System.out.println("You have one more try! Try again!");
+                guess = input.nextInt();
+                triedAttempts++;
             }
+
+        } if (triedAttempts >= 1) {
+            System.out.println("You lose! Better luck next time!");
         }
     }
 
