d5ce4de05519606b024f06b77fa2e5619c19b5c7
Kolton Koester <koltonkoester10@gmail.com>
2020-09-10 21:49:56 -0400

    finished editing 2 and 3

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -6,29 +6,30 @@ public class GuessingGameApp {
         Scanner input = new Scanner(System.in);
         System.out.println("If you need understanding on how to play enter \" 0 \".");
         System.out.println("Please guess a number 1-10: ");
-        int guess = input.nextInt();
         int triedAttempts = 0;
-        while (triedAttempts < 1){
+        int guess = input.nextInt();
+        if (guess == 0) {
+            System.out.println("You enter a number 1-10 in two tries you'll win!");
+            System.out.println("Please guess a number 1-10: ");
+            guess = input.nextInt();
+        }
+        while (triedAttempts < 2){
             if (guess == 7) {
                 System.out.println("You win!");
                 break;
-            } else if (guess == 0) {
-                System.out.println("You enter a number 1-10 until you win!");
-                System.out.println("Please guess a number 1-10: ");
-                guess = input.nextInt();
-
             } else if(guess < 0) {
                 System.out.println("You Entered A Negative Integer :( Sorry.");
                 System.out.println("GoodBye!");
                 break;
-            } else {
+            } else if (triedAttempts < 1){
                 System.out.println("You have one more try! Try again!");
                 guess = input.nextInt();
-                triedAttempts++;
+            } else if (triedAttempts == 1) {
+                System.out.println("You Lose! Come back soon!");
+            } else {
+                guess = input.nextInt();
             }
-
-        } if (triedAttempts >= 1) {
-            System.out.println("You lose! Better luck next time!");
+            triedAttempts++;
         }
     }
 
--- src/test/java/GuessingGameTest.java
+++ src/test/java/GuessingGameTest.java
@@ -1,3 +1,16 @@
-public class GuessingGameTest {
+import java.util.Scanner;
 
+public class GuessingGameTest {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+
+
+        System.out.println("what is your favorite ROYGBIV color? If you dont know what ROYGBIV means, enter \"Help\"");
+        String favoriteColor = inputScanner.nextLine();
+        if (favoriteColor.equalsIgnoreCase("Help")) {
+            System.out.println("ROYGBIV stands for: RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET");
+            System.out.println("what is your favorite ROYGBIV color?");
+            favoriteColor = inputScanner.nextLine();
+        }
+    }
 }
